a7aa4d22320696980a2999f9c5af48c8e059d344
Muskan <muskan11a4971@gmail.com>
2026-06-24 17:03:01 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        unordered_map<char, int> freq;
+        for(auto ch : s){
+            freq[ch]++;
+        }
+        for(auto ch : t){
+            freq[ch]--;
+        }
+        for(auto it : freq){
+            if(it.second != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
